555bbe41a4bf66b0d21f10d2b3728ede87f4480f
b <robbbentley1909@gmail.com>
2025-06-14 15:57:14 +0500

    group project

---
+++ src/main/java/org/example/project1/dto/SubscriptionDto.java
@@ -0,0 +1,17 @@
+package org.example.project1.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+@Data
+public class SubscriptionDto {
+    private UUID id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Integer price;
+    private boolean status;
+    private Integer duration;
+    private UserDto user;
+    private TariffDto tariff;
+}
